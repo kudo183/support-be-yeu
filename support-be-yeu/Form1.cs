8f04b0af053ba7b759ff61b359228791a0343692
phamquochuy <phamquochuy@8ec0f367-0676-cb91-5dde-23a67812e5d4>
2014-09-03 10:47:41 +0000

    - start project MaSoThueTNCN.

---
+++ support-be-yeu/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace support_be_yeu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            lblMsg.Text = "";
+
+            backgroundWorker1.DoWork += backgroundWorker1_DoWork;
+            backgroundWorker1.RunWorkerCompleted += backgroundWorker1_RunWorkerCompleted;
+        }
+
+        void backgroundWorker1_RunWorkerCompleted(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
+        {
+            txtOutput.Text = e.Result.ToString();
+            
+            lblMsg.Text = "Hoàn tất.";
+        }
+
+        void backgroundWorker1_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            var input = txtInput.Text.Split(new[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+            var processor = new TaxIDProcessor.TaxIDProcessor();
+
+            e.Result = processor.GetTaxIDs(input).Aggregate("", (current, s) => string.Format("{0}{1}\r\n", current, s));
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            lblMsg.Text = "Đang xứ lý ...";
+         
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void btnCopyToClipboard_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(txtOutput.Text);
+        }
+    }
+}
---
+++ support-be-yeu/TaxIDProcessor.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using WebserverInteractionClassLibrary;
+
+namespace TaxIDProcessor
+{
+    public class TaxIDProcessor
+    {
+        public List<string> GetTaxIDs(IEnumerable<string> personalIDs)
+        {
+            return personalIDs.Select(personalID => GetTaxID(personalID)).ToList();
+        }
+
+        public string GetTaxID(string personalID)
+        {
+            var ma = new RequestManager();
+            var url = "http://www.gdt.gov.vn/wps/portal/!ut/p/b1/04_Sj9CPykssy0xPLMnMz0vMAfGjzOINTCw9fSzCgv2dzLxdDTxDHV2NLAM8jC3CzIAKIoEKnN0dPUzMfQwMLEzcDQw8TZz8_TycAw0NPI0J6Q_XjwIrwWcCxAwcwNFA388jPzdVvyA3wiDLxFERAIgooeI!/dl4/d5/L2dJQSEvUUt3QS80SmtFL1o2XzA0OUlMOFZTT1JVMjYwSVVKU0pKQTcyS1Q1/?WCM_GLOBAL_CONTEXT=/wps/wcm/connect/sa_home";
+            var pa = string.Format("id=&page=1&action=action&mst=&fullname=&address=&cmt={0}", personalID);
+            var req = ma.GeneratePOSTRequest(url, pa, string.Empty, string.Empty, false);
+            var res = ma.GetResponse(req);
+            var content = ma.GetResponseContent(res);
+
+            var s = GetSubString(content, "<table class=\"ta_border\">", "</table>");
+
+            return ParseTable(s);
+        }
+
+        private string GetSubString(string content, string begin, string end)
+        {
+            var i = content.IndexOf(begin);
+            var temp = content.Substring(i);
+            var j = temp.IndexOf(end);
+
+            return temp.Substring(0, j + end.Length);
+        }
+
+        private string ParseTable(string table)
+        {
+            table = RemoveTagAttribute(table, "<table");
+            table = RemoveTagAttribute(table, "<th");
+            table = RemoveTagAttribute(table, "<tr");
+            table = RemoveTagAttribute(table, "<td");
+            table = RemoveTagAttribute(table, "<a");
+
+            table = table.Replace("&nbsp;", "");
+            var doc = XElement.Parse(table);
+            var nodes = doc.Descendants("a");
+
+            return string.Format("{0}\t{1}\t{2}", nodes.ElementAt(4).Value, nodes.ElementAt(1).Value, nodes.ElementAt(2).Value);
+        }
+
+        private static string RemoveTagAttribute(string table, string tag)
+        {
+            var tl = tag.Length;
+            var i = 0;
+            while (i < table.Length)
+            {
+                var begin = table.IndexOf(tag, i);
+                if (begin == -1)
+                    break;
+
+                begin = begin + tl;
+                var end = table.IndexOf(">", begin);
+                var l = end - begin;
+                if (l > 0)
+                    table = table.Replace(table.Substring(begin, l), "");
+
+                i = begin + 1;
+            }
+
+            return table;
+        }
+    }
+}
